b349ea0ff175bab8e45d523dd8d7605e01c9ee6d
Katrin  Baechli <baek@zhaw.ch>
2015-12-04 18:05:50 +0100

    working polyphonie out

--- Synthesizer_PA/source/polyphone_out.vhd
+++ Synthesizer_PA/source/polyphone_out.vhd
@@ -81,21 +81,83 @@ BEGIN
 
 input_logic: PROCESS(ALL)
 BEGIN		
-	L1:for index in 0 to 9 loop
+if (note_valid_i = '1')	then	
+
+		-- check if note is in register 		
+		if(s_note_0(7 downto 0) = note_value_i(7 downto 0) ) then			
+			s_next_index <= 0;		
+		elsif(s_note_1(7 downto 0) = note_value_i(7 downto 0) ) then			
+			s_next_index <= 1;
+		elsif(s_note_2(7 downto 0) = note_value_i(7 downto 0) ) then			
+			s_next_index <= 2;
+		elsif(s_note_3(7 downto 0) = note_value_i(7 downto 0) ) then			
+			s_next_index <= 3;
+		elsif(s_note_4(7 downto 0) = note_value_i(7 downto 0) ) then			
+			s_next_index <= 4;
+		elsif(s_note_5(7 downto 0) = note_value_i(7 downto 0) ) then			
+			s_next_index <= 5;
+		elsif(s_note_6(7 downto 0) = note_value_i(7 downto 0) ) then			
+			s_next_index <= 6;
+		elsif(s_note_7(7 downto 0) = note_value_i(7 downto 0) ) then			
+			s_next_index <= 7;
+		elsif(s_note_8(7 downto 0) = note_value_i(7 downto 0) ) then			
+			s_next_index <= 8;
+		elsif(s_note_9(7 downto 0) = note_value_i(7 downto 0) ) then			
+			s_next_index <= 9;
 		
-		-- check if note is in register 
-		if(s_dds_note_register(index)(7 downto 0) = note_value_i(7 downto 0) ) then			
-			s_next_index <= index;
-			exit L1; 			
+		-- note is new:				
+		-- look unused register
+		elsif(s_note_0(7 downto 0) = "00000000") then			
+			s_next_index <= 0;
+		elsif(s_note_1(7 downto 0) = "00000000") then			
+			s_next_index <= 1;
+		elsif(s_note_2(7 downto 0) = "00000000") then			
+			s_next_index <= 2;
+		elsif(s_note_3(7 downto 0) = "00000000") then			
+			s_next_index <= 3;			
+		elsif(s_note_4(7 downto 0) = "00000000") then			
+			s_next_index <= 4;
+		elsif(s_note_5(7 downto 0) = "00000000") then			
+			s_next_index <= 5;
+		elsif(s_note_6(7 downto 0) = "00000000") then			
+			s_next_index <= 6;
+		elsif(s_note_7(7 downto 0) = "00000000") then			
+			s_next_index <= 7;
+		elsif(s_note_8(7 downto 0) = "00000000") then			
+			s_next_index <= 8;
+		elsif(s_note_9(7 downto 0) = "00000000") then			
+			s_next_index <= 9;
+			
 		-- remember last free place in register
-		elsif(s_dds_note_register(index)(8) = '0') then
-			s_next_index <= index;
-		-- ?
+		elsif(s_note_0(8) = '0') then			
+			s_next_index <= 0;
+		elsif(s_note_1(8) = '0') then			
+			s_next_index <= 1;
+		elsif(s_note_2(8) = '0') then			
+			s_next_index <= 2;
+		elsif(s_note_3(8) = '0') then			
+			s_next_index <= 3;
+		elsif(s_note_4(8) = '0') then			
+			s_next_index <= 4;
+		elsif(s_note_5(8) = '0') then			
+			s_next_index <= 5;
+		elsif(s_note_6(8) = '0') then			
+			s_next_index <= 6;
+		elsif(s_note_7(8) = '0') then			
+			s_next_index <= 7;
+		elsif(s_note_8(8) = '0') then			
+			s_next_index <= 8;
+		elsif(s_note_9(8) = '0') then			
+			s_next_index <= 9;
 		else
 	      s_next_index <= s_index; 				
 		end if;
-	end loop;
+else
+	s_next_index <= s_index; 	
+end if;	
 end process;
+
+
 
 register_logic: process(all)
 begin
@@ -110,82 +172,41 @@ s_next_note_7 <= s_note_7;
 s_next_note_8 <= s_note_8;
 s_next_note_9 <= s_note_9;
 
-	-- change note in register, if on/off has changed
-	-- or set new note, if note is on
-	case s_index is 
-		when 0 =>
-		     
-				if s_note_0(8) /= note_value_i(8) then
-					s_next_note_0 <= note_value_i;
-				else
-					s_next_note_0 <= s_note_0;
-				end if;
-		when 1 =>
-				if s_note_1(8) /= note_value_i(8) then
-					s_next_note_1 <= note_value_i;
-				else
-					s_next_note_1 <= s_note_1;
-				end if; 
-		when 2 =>
-				if s_note_2(8) /= note_value_i(8) then
-					s_next_note_2 <= note_value_i;
-				else
-					s_next_note_2 <= s_note_2;
-				end if; 
-		when 3 =>
-				if s_note_3(8) /= note_value_i(8) then
-					s_next_note_3 <= note_value_i;
-				else
-					s_next_note_3 <= s_note_3;
-				end if; 
-		when 4 =>
-				if s_note_4(8) /= note_value_i(8) then
-					s_next_note_4 <= note_value_i;
-				else
-					s_next_note_4 <= s_note_4;
-				end if;
-		when 5 =>
-				if s_note_5(8) /= note_value_i(8) then
-					s_next_note_5 <= note_value_i;
-				else
-					s_next_note_5 <= s_note_5;
-				end if; 
-		when 6 =>
-				if s_note_6(8) /= note_value_i(8) then
-					s_next_note_6 <= note_value_i;
-				else
-					s_next_note_6 <= s_note_6;
-				end if; 
-		when 7 =>
-				if s_note_7(8) /= note_value_i(8) then
-					s_next_note_7 <= note_value_i;
-				else
-					s_next_note_7 <= s_note_7;
-				end if; 
-		when 8 =>
-				if s_note_8(8) /= note_value_i(8) then
-					s_next_note_8 <= note_value_i;
-				else
-					s_next_note_8 <= s_note_8;
-				end if; 
-		when 9 =>
-				if s_note_9(8) /= note_value_i(8) then
-					s_next_note_9 <= note_value_i;
-				else
-					s_next_note_9 <= s_note_9;
-				end if; 
+if (note_valid_i = '1') then  -- muss auf neuen index warten !! nimmt alten index
 
-		-- ?
+	case s_next_index is 
+		when 0 =>
+					s_next_note_0 <= note_value_i;
+		when 1 =>
+					s_next_note_1 <= note_value_i;
+		when 2 =>
+					s_next_note_2 <= note_value_i;
+		when 3 =>				
+					s_next_note_3 <= note_value_i; 
+		when 4 =>
+					s_next_note_4 <= note_value_i;
+		when 5 =>
+					s_next_note_5 <= note_value_i;
+		when 6 =>
+					s_next_note_6 <= note_value_i;
+		when 7 =>
+					s_next_note_7 <= note_value_i;
+		when 8 =>
+					s_next_note_8 <= note_value_i;
+		when 9 =>
+					s_next_note_9 <= note_value_i;
 		when others =>
 				s_next_note_0 <= s_note_0;
 	end case;
+else
+	s_next_note_0 <= s_note_0;
+end if;
 end process;
 
 
 ff: PROCESS(ALL)   
 BEGIN
 	if (reset_n = '0') then
-		s_dds_note_register <=  ((others => (others => '0')));
 		s_index <= 0;
 		s_note_0 <= ( OTHERS => '0' );
 		s_note_1 <= ( OTHERS => '0' );   
@@ -199,17 +220,6 @@ BEGIN
 		s_note_9 <= ( OTHERS => '0' );   
   
 	elsif (clk_12M5'event) and (clk_12M5 = '1')then
-		--s_dds_note_register <= s_next_dds_note_register;
-		s_dds_note_register(0) <= s_next_note_0; 
-		s_dds_note_register(1) <= s_next_note_1;
-		s_dds_note_register(2) <= s_next_note_2;
-		s_dds_note_register(3) <= s_next_note_3;
-		s_dds_note_register(4) <= s_next_note_4; 
-		s_dds_note_register(5) <= s_next_note_5;
-		s_dds_note_register(6) <= s_next_note_6;
-		s_dds_note_register(7) <= s_next_note_7;
-		s_dds_note_register(8) <= s_next_note_8; 
-		s_dds_note_register(9) <= s_next_note_9;
 		s_index <= s_next_index;
 		s_note_0 <= s_next_note_0;
 		s_note_1 <= s_next_note_1;
@@ -226,7 +236,6 @@ END PROCESS;
 
 
 -- signal assignment
---note_o <= s_dds_note_register;
 note_0 <= s_note_0;
 note_1 <= s_note_1;
 note_2 <= s_note_2;
